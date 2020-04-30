208bcff9f4ef8741bb86c5c89e22087ce43c0f93
revalani <renanvalani@gmail.com>
2020-04-29 22:40:20 -0300

    pre alha -4

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,8 +1,27 @@
-/* 
+/* ler mais sobre lazy load image
 
-ler mais sobre lazy load image
+https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance/images-and-video 
+https://imageresizer.com/image-compressor */
 
-https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance/images-and-video*/
+
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyBackgrounds = [].slice.call(document.querySelectorAll("aa"));
+
+  if ("IntersectionObserver" in window) {
+    let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          lazyBackgroundObserver.unobserve(entry.target);
+        }
+      });
+    });
+
+    lazyBackgrounds.forEach(function(lazyBackground) {
+      lazyBackgroundObserver.observe(lazyBackground);
+    });
+  }
+});
 
 (function ($) {
   "use strict"; // Start of use strict
@@ -21,40 +40,19 @@ https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance
     }
   });
 
-  // devido a mudança pra display flex (<922px) e blox pra normal, exigil um hidden para manter o formato
-  $('.scroll-to-top').hide();
-
-  // hidden social floating 
-  $(document).scroll(function () {
-    var scrollDistanceBot = $(this).scrollTop(),
-      elementOffset = $('#contato').offset().top,
-      distance = (elementOffset - scrollDistanceBot);
-    if (300 < distance) {
-      $('.Social-scroll').fadeIn();
-    } else {
-      $('.Social-scroll').fadeOut();
-    }
-  });
-
   // Scroll to top button appear
-  $(document).scroll(function () {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
-    } else {
-      $('.scroll-to-top').fadeOut();
-    }
-  });
+  // $(document).scroll(function () {
+  //   var scrollDistance = $(this).scrollTop();
+  //   if (scrollDistance > 100) {
+  //     $('.scroll-to-top').fadeIn();
+  //   } else {
+  //     $('.scroll-to-top').fadeOut();
+  //   }
+  // });
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
-  });
-
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 80
   });
 
   // Collapse Navbar
@@ -65,64 +63,7 @@ https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance
       $("#mainNav").removeClass("navbar-shrink");
     }
   };
-  // Collapse now if page is not at top
   navbarCollapse();
-  // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
-
-  // ajax para os pop-ups (modal) 
-  $(".list > p > a").on('click', function (event) {
-    // $(".portPopup").on('click', function (event) {
-    // debugger;
-    // event.stopPropagation();
-    // event.stopImmediatePropagation();
-
-    /*limpa informação que estavão no campos*/
-    $("#modalTitulo").empty();
-    $("#modalTexto").empty();
-    $("#carroseliIndicador").empty();
-    $("#carroselFotos").empty();
-
-    //pega o texto do campo clicado
-    var titulo = $(this).text();
-    console.log(this);
-
-
-    // coloca o titulo provisório e loading enquanto carrega o conteudo 
-    $("#modalTitulo").append(titulo);
-
-    // colocar no centro +992px
-    $(".modal-body").append('<div class="d-flex justify-content-center" id="modalLoad"><div class="spinner-border"  role="status"><span class="sr-only">Loading...</span></div></div>');
-
-
-    $.getJSON("./img/portifolio/ajaxPortifolio.php", titulo, function (dado) {
-        console.log(dado);
-      })
-      .done(function (dado) {
-        // remove loading e titulo provisório
-        $("#modalLoad").remove();
-        $("#modalTitulo").empty();
-
-        /* coloca informação*/
-        $("#modalTitulo").append(dado.titulo);
-        $("#modalTexto").append(dado.texto);
-
-        for (let index = 0; index < dado.fotos.length; index++) {
-          $("#carroseliIndicador").append('<li data-target="#carouselExampleIndicators" ></li>');
-          $("#carroselFotos").append('<div class="carousel-item"><img src="' + dado.fotos[index] + '" class="d-block w-100" alt="' + dado.fotos[index] + '"></div>');
-
-          if (index == 0) {
-            $(".carousel-item").addClass("active");
-          }
-        }
-      })
-      .fail(function () {
-        // mensagem quanto acontece erro: 
-        $("#modalTitulo").empty().append("Erro");
-        $("#modalTexto").empty().append("Não foi possível carregar o conteúdo desejado. Por favor, tente novamente.");
-        $("#modalLoad").remove();
-      });
-  });
-
 
 })(jQuery); // End of use strict
