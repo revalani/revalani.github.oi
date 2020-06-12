56bec6cf1a7c2f557edb559700e13b2f24b242b6
revalani <renanvalani@gmail.com>
2020-06-12 00:20:49 -0300

    lazy load operante e imagens+

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,28 +1,3 @@
-/* ler mais sobre lazy load image
-
-https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance/images-and-video 
-https://imageresizer.com/image-compressor */
-
-
-document.addEventListener("DOMContentLoaded", function() {
-  var lazyBackgrounds = [].slice.call(document.querySelectorAll("aa"));
-
-  if ("IntersectionObserver" in window) {
-    let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
-      entries.forEach(function(entry) {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("visible");
-          lazyBackgroundObserver.unobserve(entry.target);
-        }
-      });
-    });
-
-    lazyBackgrounds.forEach(function(lazyBackground) {
-      lazyBackgroundObserver.observe(lazyBackground);
-    });
-  }
-});
-
 (function ($) {
   "use strict"; // Start of use strict
 
@@ -39,16 +14,6 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     }
   });
-
-  // Scroll to top button appear
-  // $(document).scroll(function () {
-  //   var scrollDistance = $(this).scrollTop();
-  //   if (scrollDistance > 100) {
-  //     $('.scroll-to-top').fadeIn();
-  //   } else {
-  //     $('.scroll-to-top').fadeOut();
-  //   }
-  // });
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function () {
